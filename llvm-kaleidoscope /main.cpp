2c7650faf5f9b682ac9bec8c1b85dc7144c2babf
JanBaig <janatbaig2@gmail.com>
2025-01-08 23:14:18 -0500

    lexer start

--- llvm-kaleidoscope /main.cpp
+++ llvm-kaleidoscope /main.cpp
@@ -1,4 +1,20 @@
 
+// --- Initial Lexer --- 
+
+enum Token {
+  tok_eof = -1,
+
+  // commands
+  tok_def = -2,
+  tok_extern = -3,
+
+  // primary
+  tok_identifier = -4,
+  tok_number = -5
+};
+
+// --- Future Driver ----
+
 int main() {
     return 0;
 }
