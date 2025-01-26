6bafd8ad276a691394f9c979ac194197d19628c2
JanBaig <janatbaig2@gmail.com>
2025-01-25 20:14:39 -0500

    complete lexer

--- llvm-kaleidoscope /main.cpp
+++ llvm-kaleidoscope /main.cpp
@@ -1,3 +1,4 @@
+#include <string>
 
 // --- Initial Lexer --- 
 
@@ -12,6 +13,62 @@ enum Token {
   tok_identifier = -4,
   tok_number = -5
 };
+
+static std::string IdentifierStr; // used by tok_identifier
+static double NumVal; // used by tok_numer
+
+static int getToken() {
+  static int LastChar = ' ';
+
+  // skip whitespace
+  while (isspace(LastChar)) {
+    LastChar = getchar();
+  }
+
+  // handle alphanumeric (first char has to be alpha tho)
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+    while(isalnum(LastChar = getchar()))
+      IdentifierStr += LastChar;
+
+    if (IdentifierStr == "def") 
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern; 
+    return tok_identifier;
+  }
+
+  // handle numbers
+  if (isdigit(LastChar) || LastChar == '.') {
+    std::string NumStr;
+    do {
+      NumStr += LastChar; 
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+    
+    NumVal = strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+
+  // handle comments
+  if (LastChar == '#') {
+    do
+      LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+  
+    if (LastChar != EOF)
+      return getToken();
+  } 
+
+  // Do not consume the EOF 
+  if (LastChar == EOF) 
+    return tok_eof; 
+  
+  // return char as ASCII value
+  int ThisChar = LastChar; 
+  LastChar = getchar();
+  return ThisChar;
+}
 
 // --- Future Driver ----
 
